790e718235e11bc84073e94f825e47e8a7926dc1
NoXxChapter <ngotungchuong15@gmail.com>
2025-01-02 15:21:04 +0700

    UPDATE           + product.html, view_product.html, base.html index.html           + style.css + base.js ADD            cakeIcon.png FIX: carousel slide, product following the category
    IMPROVE: GUI

--- src/main/resources/static/js/base.js
+++ src/main/resources/static/js/base.js
@@ -1,23 +1,46 @@
-var carouselWidth = $('.carousel-inner')[0].scrollWidth;
-var cardWidth = $('.carousel-item').width();
+// Use the event handler for each carousel's next and prev buttons.
+$('.carousel').each(function () {
+    var carousel = $(this);
+    var carouselItems = carousel.find('.carousel-item');
+    var cardWidth = carouselItems.first().width();  // Get the width of a single item
+    var carouselWidth = carousel.find('.carousel-inner')[0].scrollWidth;  // Get the total scroll width
+    var scrollPosition = 0;
 
-var scrollPosition = 0;
-
-$('.carousel-control-next').on('click', function () {
-    if(scrollPosition < (carouselWidth - (cardWidth * 4))){
-        console.log('next');
-        scrollPosition += cardWidth;
-        $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600); 
+    // Calculate the number of items per slide dynamically based on carousel width
+    function calculateItemsPerSlide() {
+        return Math.floor(carousel.width() / cardWidth);  // Dynamically calculate how many items fit based on carousel width
     }
+
+    var itemsPerSlide = calculateItemsPerSlide();  // Initial calculation of items per slide
+
+    // Update the number of items per slide when the window is resized
+    $(window).on('resize', function () {
+        itemsPerSlide = calculateItemsPerSlide();
+    });
+
+    // Next button functionality
+    carousel.find('.carousel-control-next').on('click', function () {
+        if (scrollPosition < (carouselItems.length - itemsPerSlide)) {  // Check if there are more items to scroll
+            console.log('next');
+            scrollPosition += itemsPerSlide;  // Move by the number of items per slide
+            carousel.find('.carousel-inner').animate({
+                scrollLeft: scrollPosition * cardWidth  // Smooth scroll by the width of the items
+            }, 600);  // Smooth transition over 600ms
+        }
+    });
+
+    // Prev button functionality
+    carousel.find('.carousel-control-prev').on('click', function () {
+        if (scrollPosition > 0) {  // Check if we are not at the start
+            console.log('prev');
+            scrollPosition -= itemsPerSlide;  // Move back by the number of items per slide
+            carousel.find('.carousel-inner').animate({
+                scrollLeft: scrollPosition * cardWidth  // Smooth scroll by the width of the items
+            }, 600);  // Smooth transition over 600ms
+        }
+    });
 });
 
-$('.carousel-control-prev').on('click', function () {
-    if(scrollPosition > 0){
-        console.log('prev');
-        scrollPosition -= cardWidth;
-        $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600); 
-    }
-});
 
 
 function addToCart(event) {
